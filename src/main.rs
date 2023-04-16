bb1286375bf935ae75d3298eb94a750521f0cf47
DukeTresnor <armstrongbradford@gmail.com>
2023-04-16 10:11:50 -0700

    Adding initial state declarations

--- src/main.rs
+++ src/main.rs
@@ -7,3 +7,11 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .run();
 }
+
+// Declaration for MainMenu, Game, and GameOver states
+#[derive(States, Debug, Clone, Copy, Eq, PartialEq, Hash, Default)]
+pub enum AppState {
+    MainMenu,
+    Game,
+    GameOver,
+}
