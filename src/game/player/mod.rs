2ef764bc2ae9dc9b24bc9f8ec807b45b0494521e
DukeTresnor <armstrongbradford@gmail.com>
2023-04-19 23:02:49 -0700

    Adding floor

--- src/game/player/mod.rs
+++ src/game/player/mod.rs
@@ -12,12 +12,25 @@ use systems::*;
 use crate::AppState;
 use crate::game::SimulationState;
 
+
+pub const PLAYER_SPEED: f32 = 500.0;
+pub const PLAYER_SIZE: f32 = 64.0; // This is the player sprite size
+
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app
             .add_system(spawn_player.in_schedule(OnEnter(AppState::Game)))
+            .add_systems(
+                (
+                    player_movement,
+                    confine_player_movement,
+                    temp_player_up_movement,
+                )
+                .in_set(OnUpdate(AppState::Game))
+                .in_set(OnUpdate(SimulationState::Running))
+            )
             .add_system(despawn_player.in_schedule(OnExit(AppState::Game)))
             ;
     }
